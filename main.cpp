807118ba8b0cd489c55e1ce47a25aedb9f7afdc0
WVoss31 <vosswalker@gmail.com>
2022-09-23 12:22:28 -0500

    differnet materials

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 
 #include <iostream>
+#include <fstream>
 
 
 /**
@@ -46,10 +47,12 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 }
 
 int main() {
+    // File
+    std::fstream myfile;
 
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 1920;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
@@ -60,8 +63,8 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
@@ -73,9 +76,8 @@ int main() {
     camera cam;
 
     // Render
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
+    myfile.open("./image.ppm", std::fstream::out);
+    myfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
@@ -86,9 +88,9 @@ int main() {
                 ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world, max_depth);
             }
-            write_color(std::cout, pixel_color, samples_per_pixel);
+            write_color(myfile, pixel_color, samples_per_pixel);
         }
     }
-
+    myfile.close();
     std::cerr << "Done.\n";
 }
